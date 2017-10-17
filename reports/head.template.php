5909389af847d64f8b818be0028751ad232f414c
Calvin <calvin@victoriafinancial.net>
2017-10-17 15:52:44 -0700

    added unsubscribed to mailing lists

--- reports/head.template.php
+++ reports/head.template.php
@@ -4,6 +4,7 @@
     'td, th{border:1px solid #808080;padding-left:.5em;padding-right:.5em;font-size:x-small;text-align:center;}'.
     '.danger{background-color:#f2dede; color:#a94442}' .
     '.warning{background-color:#fcf8e3; color:#8a6d3b}' .
+    '.unsubscribe{font-size:x-small;text-align:center;color:grey;margin-top:1em;margin-bottom:1em;}' .
   '</style></head>';
 
 ?>
--- util/mailgun.php
+++ util/mailgun.php
@@ -11,6 +11,15 @@ function sendMail($input, $subject, $html, $class=null){
   # Instantiate the client.
   $mgClient = new MailGun(MailGunConfig::api);
   $domain = MailGunConfig::domain;
+  $unsubscribe = "";
+
+  if(isset($input['to']) ){
+    $list = explode('@', $input['to'][0]);
+    if(in_array($list[1], MailGunConfig::getMailingLists())){
+      $unsubscribe .= "<div class='unsubscribe'><a href='%mailing_list_unsubscribe_url%'>Unsubscribe from this Report</a>  |  ";
+      $unsubscribe .= "<a href='%unsubscribe_url%'>Unsubscribe from all email from ALL Reports</a></div>";
+    }
+  }
 
   $options = [];
   $options['subject'] = (isset($subject)) ? $subject : MailGunConfig::subject;
@@ -18,7 +27,8 @@ function sendMail($input, $subject, $html, $class=null){
   (isset($input['to'])) ? $options['to'] = $input['to'] : "";
   (isset($input['cc'])) ? $options['cc'] = $input['cc'] : "";
   (isset($input['bcc'])) ? $options['bcc'] = $input['bcc'] : "";
-  (isset($html)) ? $options['html'] = $html : "";
+  (isset($html)) ? $options['html'] = $html . $unsubscribe : "";
+  $options['o:require-tls'] = true;
   //var_dump($options);
 
   # Make the call to the client.
